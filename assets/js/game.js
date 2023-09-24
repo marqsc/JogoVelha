9b05b29ba6d2531a0a96138821c26fa865110e53
Cláudio Augusto <clmarques.dev@gmail.com>
2023-09-23 21:41:45 -0300

    Melhorias no Jogo da Velha
    Alterações de boas práticas feitas no css.
    Iniciando validação de winner.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,27 +1,53 @@
 
 let vez = "X";
+let currentPlayer = "X";
+
+const winningConbinations = [
+    [0, 1, 2], 
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
 
 //let caixa = document.querySelector(".caixa");
-let caixa1 = document.querySelector("#caixa1");
-let caixa2 = document.querySelector("#caixa2");
-let caixa3 = document.querySelector("#caixa3");
-let caixa4 = document.querySelector("#caixa4");
-let caixa5 = document.querySelector("#caixa5");
-let caixa6 = document.querySelector("#caixa6");
-let caixa7 = document.querySelector("#caixa7");
-let caixa8 = document.querySelector("#caixa8");
-let caixa9 = document.querySelector("#caixa9");
+const caixaElements = document.querySelectorAll(".caixa");
+
+// let caixa1 = document.querySelector("#caixa1");
+// let caixa2 = document.querySelector("#caixa2");
+// let caixa3 = document.querySelector("#caixa3");
+// let caixa4 = document.querySelector("#caixa4");
+// let caixa5 = document.querySelector("#caixa5");
+// let caixa6 = document.querySelector("#caixa6");
+// let caixa7 = document.querySelector("#caixa7");
+// let caixa8 = document.querySelector("#caixa8");
+// let caixa9 = document.querySelector("#caixa9");
 
 //Adicionando um Evento
-caixa1.addEventListener("click", acaoclick);
-caixa2.addEventListener("click", acaoclick);
-caixa3.addEventListener("click", acaoclick);
-caixa4.addEventListener("click", acaoclick);
-caixa5.addEventListener("click", acaoclick);
-caixa6.addEventListener("click", acaoclick);
-caixa7.addEventListener("click", acaoclick);
-caixa8.addEventListener("click", acaoclick);
-caixa9.addEventListener("click", acaoclick);
+for (const caixa of caixaElements) {
+    caixa.addEventListener("click", acaoclick);
+}
+
+// caixa1.addEventListener("click", acaoclick);
+// caixa2.addEventListener("click", acaoclick);
+// caixa3.addEventListener("click", acaoclick);
+// caixa4.addEventListener("click", acaoclick);
+// caixa5.addEventListener("click", acaoclick);
+// caixa6.addEventListener("click", acaoclick);
+// caixa7.addEventListener("click", acaoclick);
+// caixa8.addEventListener("click", acaoclick);
+// caixa9.addEventListener("click", acaoclick);
+
+const checkForWin = () => {
+    return winningConbinations.some((combination) => {
+        return combination.every((index) => {
+            return caixaElements[index].classList.contains(currentPlayer); // Corrigido aqui
+        });
+    });
+}
 
 function acaoclick(evento)
 {
@@ -36,5 +62,10 @@ function acaoclick(evento)
         vez = "X";
     }
 
-    clicado.removeEventListener("click", acaoclick)
+    clicado.removeEventListener("click", acaoclick);
+
+    const isWin = checkForWin();
+    if (isWin) {
+        console.log("Winner");
+    }
 }
